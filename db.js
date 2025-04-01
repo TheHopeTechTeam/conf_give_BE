08a358657f0ec42ae7fa41c23d0034fa861c057a
huichiaotsou <huichiao.tsou@gmail.com>
2025-04-01 21:47:11 +0800

    rm log

--- db.js
+++ db.js
@@ -2,8 +2,6 @@ const { Client } = require("pg");
 
 const { PGUSER, PASSWORD, HOST, PGPORT, DATABASE } = process.env;
 
-console.log("process.env: ", PGUSER, PASSWORD, HOST, PGPORT, DATABASE);
-
 const client = new Client({
   user: PGUSER,
   password: PASSWORD,
