b2c0c50804895d469ca7468a28fe5ebe39815c80
huichiaotsou <huichiao.tsou@gmail.com>
2025-04-01 21:46:30 +0800

    add client.end()

--- models/giving.js
+++ models/giving.js
@@ -1,18 +1,50 @@
-const client = require('../db')
+const client = require("../db");
 
 const givingModel = {
-    add: async(name, amount, currency, date, phone_number, email, receipt, paymentType, upload, receiptName, nationalid, company, taxid, note, cb) => {
-        await client.connect()
-        await client.query(
-            `INSERT INTO confgive (name, amount, currency, date, phone_number, email, receipt, paymentType, upload, receiptName, nationalid, company, taxid, note) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14)`,
-            [name, amount, currency, date, phone_number, email, receipt, paymentType, upload, receiptName, nationalid, company, taxid, note],
-            (err, results) => {
-                if(err) return cb(err)
-                cb(null)
-            }
-        );
-        console.log('Data insert success');
-    }
-}
+  add: async (
+    name,
+    amount,
+    currency,
+    date,
+    phone_number,
+    email,
+    receipt,
+    paymentType,
+    upload,
+    receiptName,
+    nationalid,
+    company,
+    taxid,
+    note,
+    cb
+  ) => {
+    await client.connect();
+    await client.query(
+      `INSERT INTO confgive (name, amount, currency, date, phone_number, email, receipt, paymentType, upload, receiptName, nationalid, company, taxid, note) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14)`,
+      [
+        name,
+        amount,
+        currency,
+        date,
+        phone_number,
+        email,
+        receipt,
+        paymentType,
+        upload,
+        receiptName,
+        nationalid,
+        company,
+        taxid,
+        note,
+      ],
+      (err, results) => {
+        if (err) return cb(err);
+        cb(null);
+      }
+    );
+    console.log("Data insert success");
+    client.end();
+  },
+};
 
-module.exports = givingModel
+module.exports = givingModel;
